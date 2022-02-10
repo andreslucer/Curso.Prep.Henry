4acde3b5d577725df4348829091d2831b7d8ef4a
andres <andres220799@gmail.com>
2022-02-10 21:15:29 +0100

    mi primer tp

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -26,34 +26,38 @@ const nuevoModulo = 21 % 5 === 1;
 // No cambies los nombres de las funciones.
 
 function devolverString(str) {
-  console.log("hola." + str);
+  // "return" la string provista : str
+  // tu codigo :
+  return str 
   
 }
-
-devolverString("martin");
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  return x + y
 
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  return x - y
 
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+  return x * y
 
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+  return x / y
 
 }
 
@@ -61,6 +65,9 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if ( x ==y ); {
+    return true 
+  }
 
 }
 
