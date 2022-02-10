05f7ce0b9da9434702d3a3e66d8cd41a4c035043
andres <andres220799@gmail.com>
2022-02-10 21:27:05 +0100

    mi pprimer tp

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -65,11 +65,10 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-    if (  x==y ); {
+    if ( x==y) {
       return true; 
-  }
-  return false;
-
+    }
+    return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
