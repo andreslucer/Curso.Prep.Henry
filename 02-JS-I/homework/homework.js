d6dad628f271b837615182dd2050f4e0d5afc510
andres <andres220799@gmail.com>
2022-02-12 17:40:53 +0100

    primer commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -207,9 +207,13 @@ function combinarNombres(nombre, apellido) {
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
-  // Tu código:
-
+  // Tu código:s
+  var hola = "Hola"
+  var exclamacion = "!"
+  var espacio =" "
+  return hola + espacio + nombre + exclamacion
 }
+ obtenerSaludo("martin") 
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
@@ -236,6 +240,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  var cambiodolar = euro * 1.20
+  return cambiodolar
 
 }
 
