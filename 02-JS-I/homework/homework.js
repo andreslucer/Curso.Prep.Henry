7eb2452bad0481f53cccecdf189415eae543e9f5
andres <andres220799@gmail.com>
2022-02-13 12:26:33 +0100

    andres

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -176,10 +176,16 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (numero > 0 ){
+    console.log("es mayor");
+  } else if (numero < 0){
+    console.log("es menor");
+  } else if (numero == 0){
+    return false
+  }
+ return
 }
 
-// falta terminar
 
 
 function agregarSimboloExclamacion(str) {
